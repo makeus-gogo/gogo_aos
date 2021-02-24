5d93ec58bd269425a7cb268072309de66b7113c2
imcodding <suy0226@naver.com>
2021-02-24 23:47:16 +0900

    feat: 뷰바인딩 추가

--- gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseActivity.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseActivity.java
@@ -3,8 +3,10 @@ package com.sixthank.gogo.src.common;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity<B extends ViewBinding> extends AppCompatActivity {
+    protected B binding;
 
     public void showCustomToast(final String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
