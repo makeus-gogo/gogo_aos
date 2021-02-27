7a2645fbd3de9236f596112f1bb64a53818b86d5
imcodding <suy0226@naver.com>
2021-02-27 17:54:18 +0900

    feat: 스플래시 추가

---
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/splash/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.sixthank.gogo.src.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.sixthank.gogo.R;
+import com.sixthank.gogo.databinding.ActivitySplashBinding;
+import com.sixthank.gogo.src.common.BaseActivity;
+import com.sixthank.gogo.src.main.MainActivity;
+
+public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        binding.splashTxt.startAnimation(aniFade);
+        binding.splashLogo.startAnimation(aniFade);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in_splash, R.anim.fade_out);
+                finish();
+            }
+        }, 2000);
+    }
+}
