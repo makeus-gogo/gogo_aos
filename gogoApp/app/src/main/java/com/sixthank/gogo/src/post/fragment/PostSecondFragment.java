6897412efe04121b988951235dc0a7072c8b828f
imcodding <suy0226@naver.com>
2021-03-04 22:18:54 +0900

    UI 화면설계 (3)

--- gogoApp/app/src/main/java/com/sixthank/gogo/src/post/fragment/PostSecondFragment.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/post/fragment/PostSecondFragment.java
@@ -51,30 +51,26 @@ public class PostSecondFragment extends BaseFragment<FragmentPostSecondBinding> 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPostSecondBinding.inflate(inflater, container, false);
+
         binding.postSecondRvColor.setAdapter(new ColorListAdapter());
         binding.postSecondRvTxt.setAdapter(new TextListAdapter());
-        binding.postSecondImgPicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.postSecondRvColor.setVisibility(View.GONE);
-                binding.postSecondRvTxt.setVisibility(View.GONE);
-                v.setSelected(true);
-            }
+
+        binding.postSecondImgPicture.setOnClickListener(v->{
+            binding.postSecondLlColor.setVisibility(View.GONE);
+            binding.postSecondRvTxt.setVisibility(View.GONE);
+            v.setSelected(true);
         });
-        binding.postSecondImgDraw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.postSecondRvColor.setVisibility(View.VISIBLE);
-                binding.postSecondRvTxt.setVisibility(View.GONE);
-            }
+
+        binding.postSecondImgDraw.setOnClickListener(v->{
+            binding.postSecondLlColor.setVisibility(View.VISIBLE);
+            binding.postSecondRvTxt.setVisibility(View.GONE);
         });
-        binding.postSecondImgTxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.postSecondRvColor.setVisibility(View.GONE);
-                binding.postSecondRvTxt.setVisibility(View.VISIBLE);
-            }
+
+        binding.postSecondImgTxt.setOnClickListener(v -> {
+            binding.postSecondLlColor.setVisibility(View.GONE);
+            binding.postSecondRvTxt.setVisibility(View.VISIBLE);
         });
+
         binding.postSecondNext.setOnClickListener(v -> {
            mParentActivity.switchFragment(2);
         });
