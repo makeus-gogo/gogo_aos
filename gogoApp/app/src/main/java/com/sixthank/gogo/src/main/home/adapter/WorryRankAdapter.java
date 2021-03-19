5dbb36153790c864bdbbc6b968ef59d73efe7ee8
mia <suy0226@naver.com>
2021-03-20 01:24:37 +0900

    fix: 고민 조회 TOP3 어댑터

--- gogoApp/app/src/main/java/com/sixthank/gogo/src/main/home/adapter/WorryRankAdapter.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/main/home/adapter/WorryRankAdapter.java
@@ -1,6 +1,7 @@
 package com.sixthank.gogo.src.main.home.adapter;
 
-import android.media.Image;
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +12,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.sixthank.gogo.R;
+import com.sixthank.gogo.src.common.OnItemClickListener;
+import com.sixthank.gogo.src.detail.BoardDetailActivity;
+import com.sixthank.gogo.src.main.home.models.BoardTopNResponse;
 import com.sixthank.gogo.src.main.home.models.HomeResponse;
-import com.sixthank.gogo.src.main.home.models.WorryResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WorryRankAdapter extends RecyclerView.Adapter<WorryRankAdapter.WorryHolder> {
-    List<HomeResponse.Data> list;
+    private List<BoardTopNResponse.Data> list;
+    private final Context context;
 
-    public WorryRankAdapter(List<HomeResponse.Data> list) {
+    public WorryRankAdapter(Context context, List<BoardTopNResponse.Data> list) {
+        this.context = context;
         this.list = list;
     }
 
@@ -36,11 +41,17 @@ public class WorryRankAdapter extends RecyclerView.Adapter<WorryRankAdapter.Worr
 
     @Override
     public void onBindViewHolder(@NonNull WorryHolder holder, int position) {
-        HomeResponse.Data item = list.get(position);
+        BoardTopNResponse.Data item = list.get(position);
+
         holder.worryContent.setText(item.getDescription());
-        //holder.nickName.setText()) 닉네임 줘야함
-//        holder.profile.setImageURI(Uri.parse()); 프로필 사진 넘겨줘야함
-        holder.worryImage.setImageURI(Uri.parse(item.getPictureUrl()));
+        holder.worryImage.setImageURI(Uri.parse(item.getBoardImageUrl()));
+        holder.nickName.setText(item.getNickname());
+        if(!item.getBoardImageUrl().isEmpty())
+            Glide.with(context).load(Uri.parse(item.getBoardImageUrl())).into(holder.worryImage);
+        if(item.getProfileImageUrl() != null) {
+            Glide.with(context).load(Uri.parse(item.getProfileImageUrl())).circleCrop().into(holder.profile);
+        }
+
     }
 
     @Override
@@ -49,16 +60,23 @@ public class WorryRankAdapter extends RecyclerView.Adapter<WorryRankAdapter.Worr
     }
 
     public class WorryHolder extends RecyclerView.ViewHolder {
-        TextView worryContent;
-        TextView nickName;
-        ImageView worryImage;
-        ImageView profile;
+        TextView worryContent, nickName;
+        ImageView worryImage, profile;
         public WorryHolder(@NonNull View itemView) {
             super(itemView);
             worryContent = itemView.findViewById(R.id.item_worry_rank_content);
             nickName = itemView.findViewById(R.id.item_worry_rank_nickname);
             worryImage = itemView.findViewById(R.id.item_worry_rank_img);
             profile = itemView.findViewById(R.id.item_worry_rank_profile);
+
+            itemView.setOnClickListener(v->{
+                int boardId = list.get(getAdapterPosition()).getBoardId();
+                Intent intent = new Intent(context, BoardDetailActivity.class);
+                intent.putExtra("boardId", boardId);
+                context.startActivity(intent);
+            });
         }
     }
+
+
 }
