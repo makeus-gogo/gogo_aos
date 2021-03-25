b689f28b24f6a2048d9e91e68398470a89939306
mia <55366664+imcodding@users.noreply.github.com>
2021-03-25 09:54:34 +0900

    refactor: 카카오 로그인

--- gogoApp/app/src/main/java/com/sixthank/gogo/src/login/LoginActivity.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/login/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.sixthank.gogo.src.login;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +18,7 @@ import com.sixthank.gogo.src.main.MainActivity;
 import com.sixthank.gogo.src.signup.SignUpActivity;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements LoginActivityView {
+
     private final SessionCallback mSessionCallback = new SessionCallback();
     private LoginService mLoginService;
 
@@ -29,19 +29,6 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
         setContentView(binding.getRoot());
 
         initVariable();
-
-//        binding.loginBtnKakao.setOnClickListener(v->{
-//            Log.d("KAKAO_API", "loginBtnKakaoStart");
-//            mSession.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
-//        });
-//        binding.logoutBtnKakao.setOnClickListener(v->{
-//            UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
-//                        @Override
-//                        public void onCompleteLogout() {
-//                            Log.d("KAKAO_API", "로그아웃되었습니다."); }
-//            });
-//        });
-
     }
 
     private void initVariable() {
@@ -57,7 +44,7 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // 간편로그인 실행 결과를 받아서 SDK로 전달
+        // 간편로그인
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
             Log.d("KAKAO_API", "onActivityResult");
         }
@@ -67,12 +54,13 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
     public void getLoginSuccess(LoginResponse.Data data) {
         Intent intent;
         if (data.getType().equals("SIGN_UP")) {
-            intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            intent = new Intent(getApplicationContext(), SignUpActivity.class);
             Bundle bundle = new Bundle();
             bundle.putSerializable("userInfo", data);
             intent.putExtras(bundle);
-        } else
-            intent = new Intent(LoginActivity.this, MainActivity.class);
+        } else {
+            intent = new Intent(getApplicationContext(), MainActivity.class);
+        }
 
         startActivity(intent);
     }
@@ -85,45 +73,7 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // 세션 콜백 삭제
+
         Session.getCurrentSession().removeCallback(mSessionCallback);
     }
-
-//    public class SessionCallback implements ISessionCallback {
-//        // 로그인에 성공한 상태
-//        @Override
-//        public void onSessionOpened() {
-//            requestMe();
-//        }
-//
-//        // 로그인에 실패한 상태
-//        @Override
-//        public void onSessionOpenFailed(KakaoException exception) {
-//            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
-//        }
-//
-//        // 사용자 정보 요청
-//        public void requestMe() {
-//            UserManagement.getInstance()
-//                    .me(new MeV2ResponseCallback() {
-//                        @Override
-//                        public void onSessionClosed(ErrorResult errorResult) {
-//                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
-//                        }
-//
-//                        @Override
-//                        public void onFailure(ErrorResult errorResult) {
-//                            Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
-//                        }
-//
-//                        @Override
-//                        public void onSuccess(MeV2Response result) {
-//                            String accessToken = Session.getCurrentSession().getTokenInfo().getAccessToken();
-//                            Log.i("KAKAO_API", "token: " + accessToken);
-//
-//                            mLoginService.kakaoLogin(new LoginBody(accessToken));
-//                        }
-//                    });
-//        }
-//    }
 }
