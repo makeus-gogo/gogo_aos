861bfeb3c1fe5c57ea6d6960ad57e518bed4eaeb
mia <suy0226@naver.com>
2021-03-28 23:28:56 +0900

    feat: 프로그래스바 추가

--- gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseActivity.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.sixthank.gogo.src.common;
 
+import android.graphics.drawable.ColorDrawable;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,24 @@ import java.util.HashMap;
 public class BaseActivity<B extends ViewBinding> extends AppCompatActivity {
     protected B binding;
     protected final HashMap<String, Object> mChildData = new HashMap<>();
+    protected ProgressDialog mProgressDialog;
+
 
     public void showCustomToast(final String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showProgressDialog() {
+        if(mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if(mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
--- gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseFragment.java
+++ gogoApp/app/src/main/java/com/sixthank/gogo/src/common/BaseFragment.java
@@ -1,5 +1,6 @@
 package com.sixthank.gogo.src.common;
 
+import android.graphics.drawable.ColorDrawable;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -10,9 +11,23 @@ import java.util.HashMap;
 
 public class BaseFragment<B extends ViewBinding> extends Fragment {
     protected B binding;
+    protected ProgressDialog mProgressDialog;
 
     public void showCustomToast(final String message) {
         Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    public void showProgressDialog() {
+        if(mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(getContext());
+            mProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if(mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
 }
